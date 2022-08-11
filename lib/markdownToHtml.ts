1e41553efc36ef16128cfea6300ce3e7eb74cf59
Ciobanu Laurentiu <laurentiu.ciobanu@flowos.com>
2022-08-11 14:10:55 +0300

    Code block formatting

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,18 @@
-import {remark} from "remark";
-import html from "remark-html";
+import {unified} from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+import rehypeHighlight from "rehype-highlight";
 
 export default async function markdownToHtml(markdown: string) {
-    const result = await remark().use(html).process(markdown);
+    const result = await unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeFormat)
+        .use(rehypeHighlight)
+        .use(rehypeStringify)
+        .process(markdown);
+
     return result.toString();
 }
