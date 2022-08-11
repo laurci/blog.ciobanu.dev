3e9feae69fa3698a654fee11141fd9d0be2a5ad9
Ciobanu Laurentiu <laurentiu.ciobanu@flowos.com>
2022-08-11 11:16:33 +0300

    replace icons

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import {HOME_OG_IMAGE_URL} from "../lib/constants";
 
 const Meta = () => {
     return (
@@ -8,14 +7,12 @@ const Meta = () => {
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
             <link rel="manifest" href="/favicon/site.webmanifest" />
-            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
             <link rel="shortcut icon" href="/favicon/favicon.ico" />
             <meta name="msapplication-TileColor" content="#000000" />
-            <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
             <meta name="theme-color" content="#000" />
             <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
             <meta name="description" content="I love writing code and talking about it." />
-            <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+            <meta property="og:image" content="/favicon/android-chrome-512x512.png" />
         </Head>
     );
 };
