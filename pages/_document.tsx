419520d57b8bccd50f75888ded40e811e1e7cff3
Ciobanu Laurentiu <laurentiu.ciobanu@flowos.com>
2022-08-12 13:42:11 +0300

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,22 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
+                <script defer src={`https://www.googletagmanager.com/gtag/js?id=G-SSN7HLRX7J`} />
+
+                <script
+                    defer
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){window.dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', 'G-SSN7HLRX7J', {
+                                        page_path: window.location.pathname,
+                                    });
+                `,
+                    }}
+                ></script>
+
                 <Head />
                 <body>
                     <Main />
